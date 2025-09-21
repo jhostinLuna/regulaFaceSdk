77cb72267840a6b26775c090b2966da981b58ae3
Jhostin Luna <jhostinlunadev@gmail.com>
2025-09-21 14:51:41 +0200

    refactor analize code

--- app/src/main/java/com/example/regulafacesdk/MainActivity.kt
+++ app/src/main/java/com/example/regulafacesdk/MainActivity.kt
@@ -6,23 +6,11 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.regulafacesdk.presentation.FaceSdkCamera
 import com.example.regulafacesdk.presentation.NavigationGraph
 import com.example.regulafacesdk.ui.theme.RegulaFaceSdkTheme
 import com.regula.facesdk.FaceSDK
-import com.regula.facesdk.configuration.LivenessConfiguration
-import com.regula.facesdk.enums.LivenessType
 import com.regula.facesdk.exception.InitException
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
